7fe87628a327d0eec790dd31237274a75ba10c32
jesudunyinayobolu-web-tech <jesudunyinayobolu@gmail.com>
2026-08-08 13:55:41 +0100

    ci: deploy to Netlify with static export

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 module.exports = nextConfig;
